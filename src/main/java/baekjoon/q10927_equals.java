32c49f3ada0b19e244d98fb77a8d37d3cf35ccc3
seung <seung@192.168.0.4>
2022-07-02 17:49:32 +0900

    aa

--- src/main/java/baekjoon/q10927_equals.java
+++ src/main/java/baekjoon/q10927_equals.java
@@ -8,6 +8,7 @@ public class q10927_equals {
         String a = scan.next();
         String b = "hello";
         
+        // 문자열이 같음을 나타낼 때는 .equals() 사용
         if (a.equals(b)) {
         	System.out.println(a + "!!?");
 		} else {
